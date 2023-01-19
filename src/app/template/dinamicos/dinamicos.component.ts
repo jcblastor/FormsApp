f80b03a64e48a03b22a278aaf98ab43f93db1e72
J. Carlos Guzmán <jcguzmanblack@gmail.com>
2023-01-19 14:55:09 -0300

    Formularios por template, dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -7,5 +17,31 @@ import { Component } from '@angular/core';
   ]
 })
 export class DinamicosComponent {
+  nuevoJuego: string = '';
 
+  persona: Persona = {
+    nombre: 'Carlos',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Death Stranding' },
+    ]
+  }
+
+  guardar() {
+    console.log('click')
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1)
+  }
+
+  agregarJuego() {
+    const newFavorite: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...newFavorite});
+    this.nuevoJuego = '';
+  }
 }
